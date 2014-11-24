38e98f3a15f3b4f202be3e396b516c842d0cffe3
sangnguyen110 <sangnguyen110@yahoo.com.vn>
2014-11-24 13:50:37 +0700

    initial

---
+++ font-end/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(font_end.Startup))]
+namespace font_end
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
